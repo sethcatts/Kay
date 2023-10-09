135a4b3fc0200276e2ec2027ff103906caab6979
Seth Catts <seth.h.catts@gmail.com>
2023-10-09 13:56:51 -0400

    Creating lighting cotrnol object structures

--- rfModule.py
+++ rfModule.py
@@ -1,5 +1,23 @@
 from rpi_rf import RFDevice
 
+class lightController:
+    def __init__(self):
+        light1 = light("Light 1", 0, 0)
+        light2 = light("Light 2", 0, 0)
+        light3 = light("Light 3", 0, 0)
+        print("Created light controller")
+
+class light:
+    def __init__(self, name, onCode, offCode):
+        self.name = name
+        self.onCode = onCode 
+        self.offCode = offCode
+        self.on = false
+    def isOn(self):
+        return self.on
+    def getName(self):
+        return self.name
+
 class rfController:
     def __init__(self):
         print("Created RF Controller")
@@ -14,7 +32,3 @@ class rfController:
         rfdevice.tx_repeat = 10
         rfdevice.tx_code(code, 1, 200, None)
         rfdevice.cleanup()
-
-class light:
-    def __init__(self):
-        print("Creating a light")
