4ad2ecae26bad5a3f866095cc6d198ee3ad169bf
Seth Catts <seth.h.catts@gmail.com>
2023-10-10 14:48:25 -0400

    Object class work

--- rfModule.py
+++ rfModule.py
@@ -2,6 +2,7 @@ from rpi_rf import RFDevice
 
 #lightController.light1.toggle()
 
+#TODO: Add on/off codes
 class lightController:
     def __init__(self):
         light1 = light("Light 1", 0, 0)
@@ -17,6 +18,7 @@ class light:
         self.on = false
         self.rfController = rfController()
         print("Created a light")
+
     def toggle(self):
         if(self.isOn()):
             self.rfController.sendSignal(self.offCode)
@@ -24,15 +26,16 @@ class light:
         else:
             self.rfController.sendSignal(self.onCode)
             self.on = true
+
     def isOn(self):
         return self.on
+
     def getName(self):
         return self.name
 
 class rfController:
     def __init__(self):
         print("Created RF controller object")
-
 
     def sendSignal(self, code):
         print("Sent signal: " + code)
