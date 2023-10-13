f4e63261dfdc83cee331353793c6598096c16040
Seth <seth.h.catts@gmail.com>
2023-10-13 19:29:13 -0400

    Updating class structure

--- rfModule.py
+++ rfModule.py
@@ -8,24 +8,26 @@ class lightController:
         light1 = light("Light 1", 0, 0)
         light2 = light("Light 2", 0, 0)
         light3 = light("Light 3", 0, 0)
-        print("Created light controller")
+        print("Log: Created light controller")
 
 class light:
     def __init__(self, name, onCode, offCode):
         self.name = name
         self.onCode = onCode 
         self.offCode = offCode
-        self.on = false
+        self.on = False
         self.rfController = rfController()
-        print("Created a light")
+        print("Log: Created a light")
 
     def toggle(self):
         if(self.isOn()):
             self.rfController.sendSignal(self.offCode)
-            self.on = false
+            self.on = False
+            print("Log: Toggled light off")
         else:
             self.rfController.sendSignal(self.onCode)
-            self.on = true
+            self.on = True
+            print("Log: Toggled light on")
 
     def isOn(self):
         return self.on
@@ -38,9 +40,9 @@ class rfController:
         print("Created RF controller object")
 
     def sendSignal(self, code):
-        print("Sent signal: " + code)
         rfdevice = RFDevice(17)
         rfdevice.enable_tx()
         rfdevice.tx_repeat = 10
         rfdevice.tx_code(code, 1, 200, None)
         rfdevice.cleanup()
+        print("Sent signal: " + code)
--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from rpi_rf import RFDevice
 import rfModule
 
 app = Flask (__name__)
-RFC = rfModule.rfController()
+con = rfModule.lightController()
 
 @app.route("/")
 def index():
@@ -12,25 +12,25 @@ def index():
 # background process happening without any refreshing
 @app.route('/light_one_on')
 def light_one_on():
-    RFC.sendSignal(11097756)
+    con.light1.toggle()
     return "nothing"
 
 # background process happening without any refreshing
 @app.route('/light_one_off')
 def light_one_off():
-    RFC.sendSignal(11097748)
+    con.light1.toggle()
     return "nothing"
 
 # background process happening without any refreshing
 @app.route('/light_two_on')
 def light_two_on():
-    RFC.sendSignal(11097754)
+    con.light2.toggle()
     return "nothing"
 
 # background process happening without any refreshing
 @app.route('/light_two_off')
 def light_two_off():
-    RFC.sendSignal(11097746)
+    con.light2.toggle()
     return "nothing"
 
 if __name__ == '__main__':
